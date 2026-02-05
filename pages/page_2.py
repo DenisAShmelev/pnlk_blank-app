7a646ce7f495ccbbadfc2190a05c728eaa42089e
Denis.A.Shmelev <for-denny@yandex.ru>
2026-02-05 22:49:56 +0300

    Update and rename page_2.py to pages/page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -12,3 +12,20 @@ data = pd.DataFrame(
 )
 
 st.dataframe(data, height=300)
+
+
+
+
+
+# Исходные данные
+data2 = {
+    f"Поле_{i}": [f"Значение {i}-{j}" for j in range(1, 6)]
+    for i in range(1, 11)
+}
+df = pd.DataFrame(data2)
+
+# Редактор с возможностью изменения ячеек
+edited_df = st.data_editor(df, num_rows="dynamic")
+
+st.write("Изменённые данные:")
+st.write(edited_df)
