eb7a0e5151fbc3e2d748594fef033b05cf056917
Denis.A.Shmelev <for-denny@yandex.ru>
2026-02-05 22:40:16 +0300

    Update and rename streamlit_app.py to main_app.py

--- main_app.py
+++ main_app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-#st.title("🎈 My new app")
-#st.write(
-#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-#)
-
-
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
 
 st.set_page_config(
     page_title="Моё первое Streamlit-приложение",
