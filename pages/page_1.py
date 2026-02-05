07098fda6765857d70e5331c0c9501b2ba7f16bf
Denis.A.Shmelev <for-denny@yandex.ru>
2026-02-05 22:39:09 +0300

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.title("Первая страница!")
+st.write("Здесь будет контент вашей первой страницы.")
+
+# Пример добавления графика
+import pandas as pd
+import numpy as np
+
+data = pd.DataFrame(
+    np.random.randn(50, 3),
+    columns=["A", "B", "C"]
+)
+st.line_chart(data)
